2a432cb9d9593f334fc380c2f933c220cf9314c5
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2023-11-06 22:07:45 -0300

    todas las tablas en el panel administrativo

--- proyecto_django/twm_proyecto_1/aplicacion_1/admin.py
+++ proyecto_django/twm_proyecto_1/aplicacion_1/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from aplicacion_1.models import Usuario, Equipo, Tareas, Miembros, Comunicaciones, Acciones
+
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Equipo)
+admin.site.register(Tareas)
+admin.site.register(Miembros)
+admin.site.register(Comunicaciones)
+admin.site.register(Acciones)
