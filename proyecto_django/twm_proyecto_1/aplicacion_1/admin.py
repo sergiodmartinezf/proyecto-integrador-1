0ee340d0eae720fecd62379f583590833e324c38
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2023-11-06 22:27:46 -0300

    campos en panel de administracion

--- proyecto_django/twm_proyecto_1/aplicacion_1/admin.py
+++ proyecto_django/twm_proyecto_1/aplicacion_1/admin.py
@@ -4,9 +4,27 @@ from aplicacion_1.models import Usuario, Equipo, Tareas, Miembros, Comunicacione
 
 # Register your models here.
 
-admin.site.register(Usuario)
-admin.site.register(Equipo)
-admin.site.register(Tareas)
-admin.site.register(Miembros)
-admin.site.register(Comunicaciones)
-admin.site.register(Acciones)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display=("ID","Nombre","fnac","correo","contra")
+
+class EquipoAdmin(admin.ModelAdmin):
+    list_display=("ID","nombre","descripcion")
+
+class TareasAdmin(admin.ModelAdmin):
+    list_display=("ID","Nombre","descripcion","fecha_ini","fecha_fin","ID_equipo")
+
+class MiembrosAdmin(admin.ModelAdmin):
+    list_display=("ID","ID_equipo","ID_usuario")
+
+class ComunicacionesAdmin(admin.ModelAdmin):
+    list_display=("tipo","contenido","fecha","ID_equipo","ID_usuario")
+
+class AccionesAdmin(admin.ModelAdmin):
+    list_display=("tipo","fecha","ID_equipo","ID_usuario")
+
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Equipo, EquipoAdmin)
+admin.site.register(Tareas, TareasAdmin)
+admin.site.register(Miembros, MiembrosAdmin)
+admin.site.register(Comunicaciones, ComunicacionesAdmin)
+admin.site.register(Acciones, AccionesAdmin)
