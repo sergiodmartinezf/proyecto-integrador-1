544b71ece1871598d49e9662a6ef6bd8de70e85e
Sergio Martínez <104039933+sergiodmartinezf@users.noreply.github.com>
2023-11-16 19:20:54 -0300

    carpeta JAVASCRIPT  borrada

--- proyecto_django/twm_proyecto_1/twm_proyecto_1/templates/JAVASCRIPT/navbar.js
+++
@@ -1,7 +0,0 @@
-// JavaScript en tu archivo FUNCION.JS
-const menuToggle = document.getElementById("menu-toggle");
-const menu = document.querySelector("#top-menu ul");
-
-menuToggle.addEventListener("click", function() {
-  menu.classList.toggle("show");
-});
